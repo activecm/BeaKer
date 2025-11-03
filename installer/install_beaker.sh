da59fd72b4901c26cbf01ba09bae18c98eb6ea07
moth <moth@blackhillsinfosec.com>
2025-11-03 11:57:14 -0700

    Add ssh-agent logic to better support installation using passphrase-protected SSH keys

--- installer/install_beaker.sh
+++ installer/install_beaker.sh
@@ -24,11 +24,64 @@ pushd "$SCRIPT_DIR" > /dev/null
 
 source ./helper.sh
 
+# Create a trap function to ensure that ssh-agent is stopped if it was started during installation.
+trap_actions() {
+	[ $started_ssh_agent ] && ssh-agent -k >/dev/null
+}
+
+trap trap_actions EXIT
+
 targets_split=`echo "$install_target" | sed -e 's/,/\n/g' | sort -Vu`
 
 # Fail if more than 1 target was specified and one of the targets is localhost
 if [ $(echo "$targets_split" | wc -l) -gt 1 ] && $(echo "$targets_split" | grep -Eqi '^(.+@)?(127.0.0.1|localhost|::1)$'); then
     fail "Error while processing installation targets: host list contains multiple targets and includes localhost"
+fi
+
+# Parse out only remote targets
+remote_targets=`echo "$targets_split" | grep -E -iv '^(.+@)?(127.0.0.1|localhost|::1)$' | sort -Vu`
+
+# Only start ssh-agent if the user specifies remote targets
+if [ -n "$remote_targets" ]; then
+	status "Checking for ssh-agent..."
+	
+	if [ -z "$SSH_AUTH_SOCK" ] || [ ! -S "$SSH_AUTH_SOCK" ]; then
+		status "Starting ssh-agent..."
+		eval $(ssh-agent -s) >/dev/null
+		started_ssh_agent=true
+	fi
+	
+	status "Adding SSH keys to ssh-agent..."
+
+	# Check ssh-add command output, store return code
+	set +e
+	ssh-add -q
+	ssh_add_result=$?
+	set -e
+
+	# Per ssh-add manpage: If ssh-add returns 1, command failed. If 2, failed to connect to ssh-agent.
+	# If a user is not using SSH keys, error code 1 may be nonfatal. However, error code 2 is likely fatal.
+	if [ $ssh_add_result -eq 1 ]; then
+		echo "$YELLOW"
+		echo 'NOTE: Failed to add SSH keys to ssh-agent. There may be permission issues on ~/.ssh directory/files, or SSH keys may not be present. Continuing...'
+		echo "$NORMAL"
+	elif [ $ssh_add_result -eq 2 ]; then
+		fail 'The ssh-add command was unable to connect to ssh-agent. Ensure that ssh-agent can be started by running "eval $(ssh-agent -s)"'
+	fi
+	
+	status "Checking connectivity to remote targets..."
+	
+	unreachable=()
+	for target in $remote_targets; do
+		if ! ssh $target "true" 2>/dev/null; then
+			unreachable+=($target)
+		fi
+	done
+	
+	# Fail (and print unreachable targets) if any remote targets were not reachable by SSH
+	if [ ${#unreachable[@]} -ne 0 ]; then
+		fail "The following target(s) were unreachable: ${unreachable[@]}"
+	fi
 fi
 
 bash ./ansible-installer.sh
